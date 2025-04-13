7e18c83f24105f16b297d8e83bf71dcb1697493e
mjones2453 <mjones942676@gmail.com>
2025-04-13 18:02:16 -0400

    update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -62,8 +62,8 @@ def main():
             # Handle the player actions:
             if move == 'D':
                 # Player is doubling down, they can increase their bet:
-                Additional_Bet = Get_Bet(min(bet, (money - bet)))
-                bet += Additional_Bet
+                additional_bet = Get_Bet(min(bet, (money - bet)))
+                bet += additional_bet
                 print(f'Bet increased to {bet}')
                 print('Bet:', bet)
 
@@ -104,7 +104,7 @@ def main():
         if dealer_value > 21:
             print(f'Dealer busts! You win ${bet}!')
             money += bet
-        elif (player_value > 21) or (player_value < dealer_value)
+        elif (player_value > 21) or (player_value < dealer_value):
             print('You lost!')
             money -= bet
         elif player_value > dealer_value:
@@ -117,4 +117,118 @@ def main():
         print('\n\n')
 
 def Get_Bet(max_bet):
-    """Ask the player how"""
+    """Ask the player how much they want to bet for this round."""
+    while True: # Keep asking until they enter a valid amount.
+        print(f'How much do you bet? (1-{max_bet} or QUIT)')
+        bet = input('> ').upper().strip()
+        if bet == 'QUIT':
+            print('Thanks for playing!')
+            sys.exit()
+
+        if not bet.isdecimal():
+            continue # If the player didn't enter a number, ask again.
+
+        bet = int(bet)
+        if 1 <= bet <= max_bet:
+            return bet # Player entered a valid bet.
+        
+def Get_Deck():
+    """Return a list of (rank, suit) tuples for all 52 cards."""
+    deck = []
+    for suit in (HEARTS, DIAMONDS, SPADES, CLUBS):
+        for rank in range(2, 11):
+            deck.append((str(rank), suit)) # Add the numbered cards.
+        for rank in ('J', 'Q', 'K', 'A'):
+            deck.append((rank, suit)) # Add the face and ace cards.
+    random.shuffle(deck)
+    return deck
+
+def Display_Hands(player_hand, dealer_hand, show_dealer_hand):
+    """Show the player's and dealer's cards. Hide the dealer's first
+    card if Show_Dealer_Hand is False"""
+    print()
+    if show_dealer_hand:
+        print('DEALER:', Get_Hand_Value(dealer_hand))
+        Display_Cards(dealer_hand)
+
+    else:
+        print('DEALER: ???')
+        # Hide the dealer's first card:
+        Display_Cards([BACKSIDE] + dealer_hand[1:])
+
+    # Show the player's cards:
+    print('PLAYER:', Get_Hand_Value(player_hand))
+    Display_Cards(player_hand)
+
+
+def Get_Hand_Value(cards):
+    """Returns the value of the cards. Face cards are worth 10, aces are
+    worth 11 or 1 (this function picks the most suitable ace value)."""
+    value = 0
+    number_of_aces = 0
+
+    # Add the value for the non-ace cards:
+    for card in cards:
+        rank = [0] # card is a tuple like (rank, suit)
+        if rank == 'A':
+            number_of_aces += 1
+        elif rank in ('K', 'Q', 'J'): # Face cards are worth 10 points.
+            value += 10
+        else:
+            value += int(rank) # Numbered cards are worth face value.
+
+    # Add the value for the aces:
+    value += number_of_aces # Add 1 per ace.
+    for i in range(number_of_aces):
+        # If another 10 can be added without busting, do so:
+        if value + 10 <= 21:
+            value += 10
+
+    return value
+
+def Display_Cards(cards):
+    """Display all the cards in the cards list."""
+    rows = ['', '', '', '', '',] # The text to display on each row.
+
+    for i, card in enumerate(cards):
+        rows[0] += '___ ' # Print the top line of the card.
+        if card == BACKSIDE:
+            # Print a card's back:
+            rows[1] += '|## | '
+            rows[2] += '|###| '
+            rows[3] += '|_##| '
+        else:
+            # Print the card's front:
+            rank, suit = card # The card is a tuple data structure
+            rows[1] += '|{} |'.format(rank.ljust(2))
+            rows[2] += '| {} |'.format(suit)
+            rows[3] += '|_{}|'.format(rank.rjust(2, '_'))
+
+    for row in rows:
+        print(row)
+
+
+def Get_Move(player_hand, money):
+    """Asks the player for their move, and returns 'H' for hit, 'S' for
+    stand and 'D' for double down. """
+    while True: # Keep looping until the player enters correct move.
+        # Determine what moves the player can make:
+        moves = ['(H)it', '(S)tand']
+
+        # The player can double down on their first move, which we can
+        # tell because they'll have exactly two cards:
+        if len(player_hand) == 2 and money > 0:
+            moves.append('(D)ouble down')
+
+        # Get the player's move:
+        move_prompt = ', '.join(moves) + '> '
+        move = input(move_prompt).upper()
+        if move in ('H', 'S'):
+            return move # Player has entered a valid move.
+        
+
+# If the program is run (instead of imported), run the game:
+if __name__ == '__main__':
+    main()
+
+            
